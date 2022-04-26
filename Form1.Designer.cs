8bb1ef9542f74b45fbff6ff52b80b3b48696f5de
Emre Can Serteli <epicum@gmail.com>
2022-04-26 22:42:02 +0300

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,77 @@
+﻿
+namespace Sozlukce
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.DimGray;
+            this.button1.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.button1.Font = new System.Drawing.Font("Calibri", 24F);
+            this.button1.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.button1.Location = new System.Drawing.Point(12, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(495, 128);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "BAS KANKA";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.LightGray;
+            this.ClientSize = new System.Drawing.Size(519, 152);
+            this.Controls.Add(this.button1);
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.ShowIcon = false;
+            this.SizeGripStyle = System.Windows.Forms.SizeGripStyle.Hide;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Sözlükçe Oluşturucu";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,96 @@
+﻿using ClosedXML.Excel;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Sozlukce
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        bool bitti = false;
+        string path;
+
+        private async void button1_Click(object sender, EventArgs e)
+        {
+            if (bitti)
+            {
+                System.Diagnostics.Process.Start(path);
+                MessageBox.Show("Hadi kal sağlıcakla");
+                Close();
+            }
+            else if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                button1.Text = $"Hallediyorum...";
+                path = openFileDialog1.FileName;
+                XLWorkbook workbook = null;
+                try
+                {
+                    workbook = new XLWorkbook(path);
+                }
+                catch
+                {
+                    MessageBox.Show("Bi ibnelik oldu. (Excel kapalı dimi?)");
+                    Close();
+                }
+                var worksheet = workbook.Worksheet(1);
+                var rows = worksheet.RangeUsed().RowsUsed();
+                HttpClient client = new HttpClient();
+                foreach (var row in rows)
+                {
+                    var phrase = row.Cell(1).GetString();
+
+                    HttpResponseMessage response = await client.GetAsync($"http://localhost:8080/translate?phrase={phrase}");
+                    if (response != null)
+                    {
+                        response.EnsureSuccessStatusCode();
+                        var jsonString = await response.Content.ReadAsStringAsync();
+                        var result = JsonConvert.DeserializeObject<TurengResponse>(jsonString);
+                        var i = 2;
+                        if (result.count == 0)
+                        {
+                            row.Cell(1).Style.Fill.BackgroundColor = XLColor.Red;
+                            row.Cell(1).Style.Font.FontColor = XLColor.White;
+                            row.Cell(1).Style.Font.Bold = true;
+                        }
+                        foreach (var resultPhrase in result.phrases)
+                        {
+                            row.Cell(i).Value = resultPhrase.target;
+                            i++;
+                        }
+                    }
+                }
+                workbook.Save();
+                workbook.Dispose();
+                button1.Text = $"Bitti. Bas da açayım.";
+                bitti = true;
+            }
+        }
+
+        class TurengResponse
+        {
+            public int count { get; set; }
+            public Phrase[] phrases { get; set; }
+        }
+        class Phrase
+        {
+            public string source { get; set; }
+            public string target { get; set; }
+            public string category { get; set; }
+            public string type { get; set; }
+        }
+    }
+}
